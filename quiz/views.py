33d1ce049e39e9e9b67255706705986998595198
Ian Reynolds <idreyn@gmail.com>
2016-02-22 23:18:42 +0000

    Updating

--- quiz/views.py
+++ quiz/views.py
@@ -2,7 +2,9 @@ import json
 import datetime as dt
 
 from django.http import HttpResponse
+from django.shortcuts import render
 from models import *
+from grader import decide_if_correct
 
 def json_response(obj):
 	return HttpResponse(json.dumps(obj),content_type='application/json')
@@ -21,7 +23,7 @@ def view_quiz(request,key):
 		qk = quiz_from_key(key)
 	except:
 		return error_response('invalid quiz key')
-	r = HttpResponse('Okay')
+	r = render(request,'quiz.html')
 	return r
 
 def retrieve_quiz(request,key):
@@ -49,7 +51,8 @@ def retrieve_quiz(request,key):
 		if responses.filter(question=qs).exists():
 			resp = responses.get(question=qs)
 			qd['response'] = resp.response
-			qd['correct'] = qd.correct
+			qd['explanation'] = qs.explanation
+			qd['correct'] = qs.correct
 		res['questions'].append(qd)
 	return json_response(res)
 
@@ -62,22 +65,24 @@ def accept_response(request,key):
 	if dt.datetime.now() > quiz.expires:
 		return error_response('quiz has expired')
 	achiever = qk.achiever
-	body = json.loads(request.body)
-	if not body['question']:
+	# In newer django this is request.body
+	body = json.loads(request.raw_post_data)
+	print body
+	if not body.get('question'):
 		return error_response('no question specified')
-	if not body['response']:
+	if not body.get('response'):
 		return error_response('no response specified')
-	if not Question.objects.exists(id=body['question']):
+	if not Question.objects.filter(id=body['question']).exists():
 		return error_response('question does not exist')
 	qs = Question.objects.get(id=body['question'])
-	if Response.objects.get(achiever=qk.achiever,question=qs):
+	if Response.objects.filter(achiever=qk.achiever,question=qs).exists():
 		return error_response('question has already been answered')
 	resp = Response(
 		quiz=quiz,
 		achiever=achiever,
 		question=qs,
 		response=body['response'],
-		is_correct=decide_if_correct(question,body['response'])
+		is_correct=decide_if_correct(qs,body['response'])
 	)
 	resp.save()
 	return json_response({
